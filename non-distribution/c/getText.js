60e27c13e0b9c0df46041ac7f598b4ebeaca085c
Yash Agrawal <yagrawal2023@gmail.com>
2025-01-25 18:43:50 +0000

    getText.js

--- non-distribution/c/getText.js
+++ non-distribution/c/getText.js
@@ -12,12 +12,17 @@ const rl = readline.createInterface({
   input: process.stdin,
 });
 
+let text = "";
+
 rl.on('line', (line) => {
   // 1. Read HTML input from standard input, line by line using the `readline` module.
+  text += line;
 });
 
 // 2. after all input is received, use convert to output plain text.
 rl.on('close', () => {
+  console.log(convert(text));
+  text = "";
 });
 
 
