82643483367ce10d2c5837afa58f2d552b522395
Yash Agrawal <yagrawal2023@gmail.com>
2025-01-29 01:26:01 +0000

    perf index

--- non-distribution/p/perf-index.sh
+++ non-distribution/p/perf-index.sh
@@ -2,7 +2,11 @@
 
 cd "$(dirname "$0")/.." || exit 1
 
-MAX_URLS=10
+MAX_URLS=${1:-0}
+if ! [[ "$MAX_URLS" =~ ^-?[0-9]+$ ]]; then
+  echo "Error: Argument is not a valid integer"
+  exit 1
+fi
 ./p/get-content.sh $MAX_URLS
 
 INDEXED_URLS=0
@@ -10,12 +14,10 @@ START_TIME=$(date +%s)
 
 while [[ $INDEXED_URLS -lt $MAX_URLS ]]; do
   url=$(cat "p/d/url_$INDEXED_URLS.txt")
-  echo "indexing $url"
-  ./index.sh "p/d/content_$INDEXED_URLS.txt" "$url"
+  # echo "indexing $url"
+  ./index.sh "p/d/content_$INDEXED_URLS.txt" "$url" < /dev/null
   ((INDEXED_URLS++))
 done
-
-echo "outside loop"
 
 END_TIME=$(date +%s)
 SECONDS_ELAPSED=$(( END_TIME - START_TIME ))
@@ -26,6 +28,6 @@ SECS_PER_URL=$(awk -v a="$SECONDS_ELAPSED" -v b="$INDEXED_URLS" 'BEGIN { print a
 echo "Throughput: $URLS_PER_SEC urls/sec"
 echo "Latency: $SECS_PER_URL secs/url"
 
-./p/delete-content.sh
+./p/delete.sh
 
 exit 0;
