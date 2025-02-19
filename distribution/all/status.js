2844611831cc15f9dde7cecb43d05c6ebc7d3c9f
Yash Agrawal <yagrawal2023@gmail.com>
2025-02-19 20:44:08 +0000

    status all first pass

--- distribution/all/status.js
+++ distribution/all/status.js
@@ -5,12 +5,56 @@ const status = function(config) {
 
   return {
     get: (configuration, callback) => {
+      // Handle parameters
+      configuration = configuration || '';
+      callback = callback || console.log;
+      if (typeof configuration != 'string' || typeof callback != 'function') {
+        callback(new Error('Invalid parameters'), null);
+        return;
+      }
+
+      global.distribution[context.gid].comm.send([configuration], {service: 'status', method: 'get'}, (e, v) => {
+        if (e instanceof Error) {
+          callback(e, null);
+          return;
+        }
+
+        if (configuration == 'counts' || configuration == 'heapTotal' || configuration == 'heapUsed') {
+          let total = 0;
+          for (let value of Object.values(v)) {
+            total += value;
+          }
+          callback(e, total);
+        } else {
+          callback(e, v);
+        }
+      })
     },
 
     spawn: (configuration, callback) => {
+      // Handle parameters
+      configuration = configuration || '';
+      callback = callback || console.log;
+      if (typeof configuration != 'string' || typeof callback != 'function') {
+        callback(new Error('Invalid parameters'), null);
+        return;
+      }
+
+      // Call spawn on each node
+      global.distribution[context.gid].comm.send([configuration], {service: 'status', method: 'spawn'}, callback);
     },
 
     stop: (callback) => {
+      // Handle parameters
+      configuration = configuration || '';
+      callback = callback || console.log;
+      if (typeof configuration != 'string' || typeof callback != 'function') {
+        callback(new Error('Invalid parameters'), null);
+        return;
+      }
+
+      // Call stop on each node
+      global.distribution[context.gid].comm.send([configuration], {service: 'status', method: 'stop'}, callback);
     },
   };
 };
