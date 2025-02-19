4e89c7bd09c3942c0d781498395363548f883a84
Yash Agrawal <yagrawal2023@gmail.com>
2025-02-19 19:42:33 +0000

    first all comm attempt

--- distribution/all/comm.js
+++ distribution/all/comm.js
@@ -1,5 +1,9 @@
 /** @typedef {import("../types").Callback} Callback */
 
+const groups = require('../local/groups');
+const id = require('../util/id');
+const { send } = require('../local/comm');
+
 /**
  * NOTE: This Target is slightly different from local.all.Target
  * @typdef {Object} Target
@@ -21,6 +25,54 @@ function comm(config) {
    * @param {Callback} callback
    */
   function send(message, configuration, callback) {
+    // Handle parameters
+    message = message || [];
+    callback = callback || function() { };
+    if (!Array.isArray(message) || typeof callback != 'function' || typeof configuration != 'object') {
+      callback(new Error('Invalid parameters'), null);
+      return;
+    }
+
+    /*
+      Idea - have the callback consume an index and value/error objects to accumulate in, 
+      if index == groups.length call the actual callback with value/error objects,
+      if index < groups.length call local.comm.send to node that corresponds to index passed in,
+      and in the callback for that, append error/value to object, then call callback itself with index incremented
+
+      Can initiate recursive calls by calling the callback itself with cb(0, {}, {});
+
+      This all must be done inside a callback to group.get so that it can access group size/list
+    */
+    groups.get(gid, (e, v) => {
+      // Handle error
+      if (e) {
+        callback(e, null);
+        return;
+      }
+
+      // Create callback
+      const nodes = v;
+      const numNodes = nodes.length;
+      const cb = (index, errors, values) => {
+        if (index == numNodes) {
+          callback(errors, values);
+        } else {
+          const node = nodes[index];
+          send(message, configuration, (e, v) => {
+            const sid = id.getSID(node)
+            if (e) {
+              errors[sid] = e;
+            } else {
+              values[sid] = v;
+            }
+            cb(index + 1, errors, values);
+          });
+        }
+      }
+      
+      // Execute callback logic
+      cb(0, {}, {});
+    });
   }
 
   return {send};
