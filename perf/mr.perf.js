105f1a13a2b95d5425ed21426d81851ae89bfe12
Yash Agrawal <yagrawal2023@gmail.com>
2025-03-23 04:20:21 +0000

    code m5

---
+++ perf/mr.perf.js
@@ -0,0 +1,142 @@
+const distribution = require('../config.js');
+const id = distribution.util.id;
+
+const cfreqGroup = {};
+
+/*
+    The local node will be the orchestrator.
+*/
+let localServer = null;
+
+const n1 = {ip: '127.0.0.1', port: 7110};
+const n2 = {ip: '127.0.0.1', port: 7111};
+const n3 = {ip: '127.0.0.1', port: 7112};
+
+test('(25 pts) all.mr:cfreq', (done) => {
+  // Calculate the frequency of each character in a set of documents
+  const mapper = (key, value) => {
+    const chars = value.replace(/\s+/g, '').split('');
+    const out = [];
+    chars.forEach((char) => {
+      const o = {};
+      o[char] = 1;
+      out.push(o);
+    });
+    return out;
+  };
+
+  const reducer = (key, values) => {
+    const out = {};
+    out[key] = values.reduce((sum, v) => sum + v, 0);
+    return out;
+  };
+
+  const dataset = [
+    {'doc1': 'hello world'},
+    {'doc2': 'map reduce test'},
+    {'doc3': 'character counting example'},
+  ];
+
+  const expected = [
+    {'h': 2}, {'e': 7}, {'l': 4},
+    {'o': 3}, {'w': 1}, {'r': 4},
+    {'d': 2}, {'m': 2}, {'a': 4},
+    {'p': 2}, {'u': 2}, {'c': 4},
+    {'t': 4}, {'s': 1}, {'n': 2},
+    {'i': 1}, {'g': 1}, {'x': 1},
+  ];
+
+  let totalCharacters = 0;
+  for (let kv of expected) {
+    totalCharacters += Object.values(kv)[0];
+  }
+
+  function logPerf(start, end) {
+    const time = end - start;
+    const throughput = totalCharacters / time;
+    const latency = time / totalCharacters;
+
+    console.log(`Throughput: ${throughput} items/ms`);
+    console.log(`Latency: ${latency} ms/item`);
+  }
+
+  const doMapReduce = (cb) => {
+    const startTime = performance.now();
+    distribution.cfreq.mr.exec({keys: getDatasetKeys(dataset), map: mapper, reduce: reducer}, (e, v) => {
+      try {
+        const endTime = performance.now();
+        logPerf(startTime, endTime);
+        expect(v).toEqual(expect.arrayContaining(expected));
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  };
+
+  let cntr = 0;
+
+  dataset.forEach((o) => {
+    const key = Object.keys(o)[0];
+    const value = o[key];
+    distribution.cfreq.store.put(value, key, (e, v) => {
+      cntr++;
+      if (cntr === dataset.length) {
+        doMapReduce();
+      }
+    });
+  });
+});
+
+/*
+    Test setup and teardown
+*/
+
+// Helper function to extract keys from dataset (in case the get(null) funnctionality has not been implemented)
+function getDatasetKeys(dataset) {
+  return dataset.map((o) => Object.keys(o)[0]);
+}
+
+beforeAll((done) => {
+  cfreqGroup[id.getSID(n1)] = n1;
+  cfreqGroup[id.getSID(n2)] = n2;
+  cfreqGroup[id.getSID(n3)] = n3;
+
+  const startNodes = (cb) => {
+    distribution.local.status.spawn(n1, (e, v) => {
+      distribution.local.status.spawn(n2, (e, v) => {
+        distribution.local.status.spawn(n3, (e, v) => {
+          cb();
+        });
+      });
+    });
+  };
+
+  distribution.node.start((server) => {
+    localServer = server;
+
+    const cfreqConfig = {gid: 'cfreq'};
+    startNodes(() => {
+      distribution.local.groups.put(cfreqConfig, cfreqGroup, (e, v) => {
+        distribution.cfreq.groups.put(cfreqConfig, cfreqGroup, (e, v) => {
+          done();
+        });
+      });
+    });
+  });
+});
+
+afterAll((done) => {
+  const remote = {service: 'status', method: 'stop'};
+  remote.node = n1;
+  distribution.local.comm.send([], remote, (e, v) => {
+    remote.node = n2;
+    distribution.local.comm.send([], remote, (e, v) => {
+      remote.node = n3;
+      distribution.local.comm.send([], remote, (e, v) => {
+        localServer.close();
+        done();
+      });
+    });
+  });
+});
