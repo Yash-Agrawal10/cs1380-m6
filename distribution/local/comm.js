f67078b7f6758a8f8b4be2a065f63101a52c208d
Joncarlo Migaly <joncarlomigaly@Joncarlos-MacBook-Air.local>
2025-04-01 14:30:14 -0400

    subbed in JC's local comm.js

--- distribution/local/comm.js
+++ distribution/local/comm.js
@@ -18,53 +18,54 @@ const {serialize, deserialize} = require('../util/serialization');
  * @return {void}
  */
 function send(message, remote, callback) {
-  // Handle parameters
-  message = message || [];
-  callback = callback || function() { };
+  message = message || []
 
-  // Set up message
-  const serializedArgs = serialize(message);
-  const gid = remote.gid || 'local';
-  const path = `/${gid}/${remote.service}/${remote.method}`;
-  const options = {
-    hostname: remote.node.ip,
-    port: remote.node.port,
-    path: path,
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(serializedArgs),
-    },
-  };
-
-  // Create request
-  const req = http.request(options, (res) => {
-    let body = '';
-
-    res.on('data', (chunk) => {
-      body += chunk;
-    });
-
-    res.on('end', () => {
-      const data = deserialize(body);
-      // Debugging
-      if (res.statusCode >= 200 || res.statusCode < 300) {
-        const error = data.error;
-        const value = data.value;
-        callback(error, value);
-      } else {
-        const message = `Request failed with status code ${req.statusCode}: ${data}`;
-        callback(new Error(message), null);
+  // Serialize the message
+  let serializedMessage = serialize(message)
+  let gid = remote.gid || 'local'
+  // Build path
+  let path = '/' + gid + '/' + remote.service + '/' + remote.method
+  // Set up options object for http.request
+  let options = {
+      hostname: remote.node.ip,
+      port: remote.node.port,
+      path: path,
+      method: 'PUT',
+      headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(serializedMessage)
       }
-    });
-  });
+  }
 
-  // Send request
-  req.on('error', (e) => {
-    callback(e, null);
-  });
-  req.write(serializedArgs);
-  req.end();
+  const req = http.request(options, (res) => {
+      let data = ''
+
+      // Handle data chunks
+      res.on('data', (chunk) => {
+          data += chunk
+      })
+
+      // Handle end of response
+      res.on('end', () => {
+          let trueData = deserialize(data)
+          if (res.statusCode >= 200 || res.statusCode < 300) {
+              let error = trueData.error
+              let value = trueData.value
+              callback(error, value)
+              return
+          }
+          const message = `Request failed with status code ${req.statusCode}: ${data}`;
+          callback(new Error(message), null);
+      })
+  })
+
+  // Handle network errors
+  req.on('error', (err) => {
+      callback(err)  // Pass the error to the callback
+  })
+
+  req.write(serializedMessage)
+  req.end()
 }
 
 module.exports = {send};
