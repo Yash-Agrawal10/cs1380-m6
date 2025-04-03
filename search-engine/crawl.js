3e75b0904d492c16e80b4eaf41216d234c1399fe
Yash Agrawal <yagrawal2023@gmail.com>
2025-04-03 17:22:48 -0400

    nodes

---
+++ search-engine/crawl.js
@@ -0,0 +1,8 @@
+const crawl = (url) => {
+    /* 
+        1. Download url content
+        2. Store url content on indexers
+        3. Extract new urls
+        4. Extend to-crawl list with new urls
+    */
+}
